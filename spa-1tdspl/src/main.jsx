617f3aa27f5bc482cc1c255cfbd312358fce34ac
NeugeMa <mariana.n.dourado@gmail.com>
2023-08-31 08:56:54 -0300

    iniciando roulters, createBrowserRouter, Objetos de rotas + README

--- spa-1tdspl/src/main.jsx
+++ spa-1tdspl/src/main.jsx
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Bloco de ROTAS 
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'; 
+import EditarProdutos from './routes/EditarProdutos.jsx'; 
+import Erro404 from './routes/Erro404.jsx';
+import Home from './routes/Home.jsx'; 
+import Produtos from './routes/Produtos.jsx';
+
+const router = createBrowserRouter([ 
+  { path: '', element: <App/>, 
+    children: [
+      { path: '/', elements: <Home /> } 
+      { path: '/produtos', elements: <Produtos /> } 
+      { path: '/editar/produtos/:id', elements: <EditarProdutos /> } 
+    ] }
+]);
+
+
+// Bloco de ROTAS 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
