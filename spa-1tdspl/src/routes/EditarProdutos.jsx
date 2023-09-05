ba4bd181f070dc042b914985b9d712c5cc86052e
NeugeMa <mariana.n.dourado@gmail.com>
2023-09-05 10:28:55 -0300

    fim da aula

--- spa-1tdspl/src/routes/EditarProdutos.jsx
+++ spa-1tdspl/src/routes/EditarProdutos.jsx
@@ -1,5 +1,48 @@
-export default function EditarProduto() { 
-    return ( 
-        <div>EditarProduto</div>
+import { useParams } from "react-router-dom"
+import { ListaProdutos } from "../components/ListaProdutos"
+
+export default function EditarProdutos() {
+    
+    document.title = "Editar Produtos";
+  
+    //Receber o ID do produto pelo HOOK useParams( );
+          const {id} = useParams(); 
+
+    // Recuperar o produto na lista pelo ID. 
+    const produtoRecuperado = ListaProdutos.filter(produto => produto.id == id ); 
+
+    return (
+        <div>
+            <h1> Editar Produtos </h1>
+            <p>Descrição  : {produtoRecuperado[0].desc } </p>
+            <p>Preço 🤑 : {produtoRecuperado[0].preco } </p>
+
+                <di> 
+                    <form> 
+                        <fieldset> 
+                            <legend> Produto a ser Editado </legend>
+                                <div> 
+                                    <label htmlFor="">Nome: </label>
+                                    <input type="text" name="nome" id="idNome" defaultValue={produtoRecuperado[0].nome}/>
+                                </div> 
+
+                                <div> 
+                                    <label htmlFor="">Descrição: </label>
+                                    <input type="text" name="desc" id="idDesc" defaultValue={produtoRecuperado[0].desc}/>
+                                </div> 
+                                
+                                <div> 
+                                    <label htmlFor="">Preço: </label>
+                                    <input type="text" name="preco" id="idPreco" defaultValue={produtoRecuperado[0].preco}/>
+                                </div> 
+
+                                    <div> 
+                                        <button> Editar </button>
+                                    </div>
+
+                        </fieldset>
+                    </form>
+                </di>
+        </div>
     )
-}
+  }
