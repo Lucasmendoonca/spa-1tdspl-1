2c6609af857f50e32cb3f79773265730f2bb1b9e
Mariana Neugebauer Dourado <126569669+NeugeMa@users.noreply.github.com>
2023-09-06 17:55:33 -0300

    Update Home.jsx

--- spa-1tdspl/src/routes/Home.jsx
+++ spa-1tdspl/src/routes/Home.jsx
@@ -1,7 +1,7 @@
 export default function Home() { 
     return ( 
         <div>
-            <h1> MINHA CASA </h1>
+            <h1> MINHA CASAAAAAAAA </h1>
             <p> Seja bem-vindo!! </p>
         </div>
     )
